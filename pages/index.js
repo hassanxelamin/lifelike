baccecc3683080f70c698a00e0ed9a0051efd248
hassanxelamin <accounts@hassanxelamin.com>
2022-12-16 13:40:28 -0800

    testing performance - added strict mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React, {StrictMode} from 'react'
-import Logo from '../components/Logo'
-import { Canvas } from '@react-three/fiber'
 import Link from 'next/link'
+import { Canvas } from '@react-three/fiber'
+
+import Logo from '../components/Logo'
 
 export default function Landing() {
 
@@ -13,15 +14,17 @@ export default function Landing() {
   }
 
   return (
-    <div className="font-grotesk h-full w-full absolute left-0 top-0 overflow-hidden flex flex-col items-center justify-center">
-        <div className='h-[170px] sm:h-[200px] w-[400px]'>
-          <Canvas camera={cameraSettings}>
-            <Logo />
-          </Canvas>
-        </div>
-        <Link href='/home'>
-          <button className='text-[15px] text-bold w-[150px] h-[50px] border-black border-1 border-solid'>Enter</button>
-        </Link>
-    </div>
+    <StrictMode>
+      <div className="font-grotesk h-full w-full absolute left-0 top-0 overflow-hidden flex flex-col items-center justify-center">
+          <div className='h-[170px] sm:h-[200px] w-[400px]'>
+            <Canvas camera={cameraSettings}>
+              <Logo />
+            </Canvas>
+          </div>
+          <Link href='/home'>
+            <button className='text-[15px] text-bold w-[150px] h-[50px] border-black border-1 border-solid'>Enter</button>
+          </Link>
+      </div>
+    </StrictMode>
   )
 }
